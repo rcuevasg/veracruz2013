028f6ea980f9141b3e019a4407ec7b65d1e362b6
rdrouaillet <rol_dropu@hotmail.com>
2013-08-30 18:44:59 -0500

    Modificación al front page
    Se cambio la estructura del frontpage para colocar los post en columnas

--- pagetemplate-frontpage.php
+++ pagetemplate-frontpage.php
@@ -5,7 +5,7 @@ Template Name: Front page
 ?>
 <?php get_header(); ?>
 				<section id="principalContent" class="container">
-				<section class="col-lg-8">
+				<section class="col-lg-8 post-home">
 						 <?php
 						$categoriaBlog = get_category_by_slug('blog');
 						$categoriaBlog = $categoriaBlog->term_id;
@@ -15,7 +15,7 @@ Template Name: Front page
 						 	$blog_query->the_post();
 						 	$wp_query->in_the_loop = true;
 						 ?>
-                        <div class="post row-fluid mover-der">
+                        <div class="lista-post-home col-lg-12">
                             <?php
 								$domsxe = simplexml_load_string(get_the_post_thumbnail($post->ID, 'full'));
 					    		$thumbnailsrc = "";
@@ -29,19 +29,19 @@ Template Name: Front page
 							   
 								if (!empty($thumbnailsrc)):
 								?>
-									<div class="">
+									<div class="col-md-6">
 								 	<span class='img img-responsive'><img class="img-responsive" src='<?php bloginfo('template_url') ?>/timthumb.php?src=<?php print $thumbnailsrc; ?>&w=255&h=160&a=cr' border=0 /></span>
 								 	</div>
 								 <?php
 								 endif;
 								 ?>
-                            <div class="post-summary span6">
+                            <div class="col-md-6">
                                 <div class="post-date"><?php the_time( 'j M' ); ?></div>
-                                <h1><?php the_title(); ?></h1>
+                                <h3><?php the_title(); ?></h3>
                                 <?php the_excerpt(); ?>
                                 <!--<a href="<?php the_permalink(); ?>" class="btn read-more">Leer Más</a> -->
-                            </div> <!-- Terminar DIV post-summary span6 -->
-                        </div> <!-- Terminar DIV post row-fluid mover-der -->
+                            </div>
+                        </div> <!-- Terminar DIV post -->
                     <?php 
 					endwhile;  //Terminar while de post dentro de BLOG
 					wp_reset_query();
@@ -49,6 +49,7 @@ Template Name: Front page
 						//wp_pagenavi( array( 'query' => $blog_query ) );
 					//endif; 
 					//PAGINACION
+					//
 					?>
                  </section>
 <?php get_sidebar(); ?>
