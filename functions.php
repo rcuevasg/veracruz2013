b09ddc2dc0d2332f62e476f871c3c7e32f76c23f
rdrouaillet <rol_dropu@hotmail.com>
2013-09-02 18:05:06 -0500

    se agrega clase btn-default a btn leer más
    se agrego clase a la función de leer más en el archivo functions.php

--- functions.php
+++ functions.php
@@ -89,7 +89,7 @@ add_theme_support('automatic-feed-links');
 // Replaces the excerpt "more" text by a link
 function new_excerpt_more($more) {
        global $post;
-	return '<a class="moretag btn read-more" title="'.get_the_title($post->ID).'" href="'. get_permalink($post->ID) . '"> Leer m&aacute;s</a>';
+	return '<a class="moretag btn btn-default read-more" title="'.get_the_title($post->ID).'" href="'. get_permalink($post->ID) . '"> Leer m&aacute;s</a>';
 }
 add_filter('excerpt_more', 'new_excerpt_more');
 
