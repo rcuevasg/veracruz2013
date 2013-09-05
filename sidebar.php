4bc397f4ca03c4d975d02226fcc60d860a3f631c
rdrouaillet <rol_dropu@hotmail.com>
2013-09-04 20:15:07 -0500

    Cambios en el sidebar para div atención ciudadana
    se cambio el comportamiento del div para atencion ciudadana

--- sidebar.php
+++ sidebar.php
@@ -9,21 +9,19 @@
 ?>
 
 <aside id="sidebar" class="col-md-4 col-lg-4" >
-			
+<center>
 <?php
 	/* When we call the dynamic_sidebar() function, it'll spit out
 	 * the widgets for that widget area. If it instead returns false,
 	 * then the sidebar simply doesn't exist, so we'll hard-code in
 	 * some default sidebar stuff just in case.
 	 */
+	
 	if ( is_active_sidebar( 'sidebar-widget-area' ) ) : ?>
 
-			<ul class="widget-list">
+			<div class="widget-list">
 				<?php dynamic_sidebar( 'sidebar-widget-area' ); ?>
-			</ul>
+			</div>
 <?php endif; ?>
-<div class="cajaTextoSide">
-        <h4>Atención Ciudadana</h4>
-        <center><a href="#" class="btn btn-default">Contáctanos</a></center>
-    </div>
+</center>
 </aside>
