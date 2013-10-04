0bf85a392db73427eaf1674104c2781b90cfe88e
rdrouaillet <rol_dropu@hotmail.com>
2013-10-04 14:22:04 -0500

    Nada
    NAda de NADA

--- pagetemplate-prensa.php
+++ pagetemplate-prensa.php
@@ -61,6 +61,7 @@ if(	$nombreparent==$padre){
 	<h3><?php echo $page->post_title; ?></h3>
 		<div class="entry"><?php echo substr($content, 0, 100); ?></div>
         <?php 
+		
 		$nombreCategory=get_the_title($page->ID);
 		$categoriaBlog = get_category_by_slug($nombreCategory);
 		$categoriaBlog = $categoriaBlog->term_id;
